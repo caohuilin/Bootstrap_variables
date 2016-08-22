a2fbac2bf4bfbb4657d9fe398cd3855bd580ec1e
caohuilin <caohuilin9588@foxmail.com>
2016-08-22 11:02:52 +0800

    优化脚本和文件组织结构，让项目更清晰

--- README.md
+++ README.md
@@ -1,12 +1,22 @@
-# Bootstrap_variables
-修改bootstrap的变量文件，重新编译
+# My-Bootstrap
+创建拥有自己独特风格的网站,是每个人的梦想。
+尽可能用更少的代码是每个程序员的追求。
+拥有开源并独具特色的bootstrap,让你的期望成为可能。
 
 ## Install
- npm i
- 
- bower i
-## Run 
- gulp serve
- 
-## Change
-改变src/variables 刷新页面即可
+
+```
+$ npm install
+```
+
+## Run
+```
+$ gulp dev
+```
+
+## Build
+```
+$ gulp build
+```
+
+#### 通过修改variables.less文件,将bootstrap换个想要的皮肤,通过添加theme.less文件,定义属于自己的组件样式。创建属于自己风格的组件库,行动起来吧!
--- gulpfile.js
+++ gulpfile.js
@@ -1,58 +1,88 @@
 var gulp = require('gulp');
-var postcss = require('gulp-postcss');
-var less = require('gulp-less');
-var autoprefixer = require('autoprefixer');
-var serve = require('gulp-serve');
-var path = require('gulp-path');
-var concat = require('gulp-concat');
-var clean = require('gulp-clean');
-var runSequence = require('run-sequence');
+var del = require('del');
 var browserSync = require('browser-sync');
+var watch = require('gulp-watch');
+var autoprefixer = require('autoprefixer');
+var plumber = require('gulp-plumber');
+var less = require('gulp-less');
+var postcss = require('gulp-postcss');
+var concat = require('gulp-concat');
+var minifyCss = require('gulp-minify-css');
+var rename = require('gulp-rename');
+var htmlReplace = require('gulp-html-replace');
+var runSequence = require('run-sequence');
 
-gulp.task('copy_bootstrap',function(){
-    return gulp.src('./bower_components/bootstrap/less/*')
-        .pipe(gulp.dest('./dist/btcc_bootstrap'));
+gulp.task('clean', function () {
+    del.sync(["dist", "app", './src/my_bootstrap'])
 });
 
-gulp.task('copy_mixins',function(){
-    return gulp.src('./bower_components/bootstrap/less/mixins/*')
-        .pipe(gulp.dest('./dist/btcc_bootstrap/mixins'));
+gulp.task('browser_sync', function () {
+    browserSync({
+        server: {
+            baseDir: './app',
+            index: 'index.html',
+        }
+    })
 });
 
-gulp.task("copy_variables",function(){
-    return gulp.src("./src/*")
-        .pipe(gulp.dest('./dist/btcc_bootstrap'));
+gulp.task('watch', function () {
+    watch('src/*.less', function () {
+        gulp.watch('copy_theme', 'less');
+    });
 });
 
-gulp.task("less",function(){
+gulp.task('copy_bootstrap', function () {
+    return gulp.src(['./node_modules/bootstrap/less/**/*'], {base:'./node_modules/bootstrap/less'})
+        .pipe(gulp.dest('./src/my_bootstrap/'));
+});
+
+gulp.task('copy_theme', function () {
+    return gulp.src(['./src/theme.less', './src/variables.less'], {base:'./src'})
+        .pipe(gulp.dest('./src/my_bootstrap/'));
+});
+
+gulp.task("less", function() {
     var processors = [
         autoprefixer
     ];
-    return gulp.src('./dist/btcc_bootstrap/bootstrap.less')
+    return gulp.src(['./src/my_bootstrap/bootstrap.less', './src/my_bootstrap/theme.less'])
+        .pipe(plumber())
         .pipe(less())
         .pipe(postcss(processors))
-        .pipe(gulp.dest('./dist'));
+        .pipe(gulp.dest('./app/css/'));
 });
 
-gulp.task('clean',function(){
-   return gulp.src('./dist/btcc_bootstrap',{read:false})
-       .pipe(clean());
+gulp.task('copy', function () {
+    return gulp.src(['./src/fonts/**/*', './src/index.html'], {base:'./src'})
+        .pipe(gulp.dest('./app/'));
 });
 
-gulp.task('build',function(){
-    runSequence('copy_bootstrap','copy_mixins','copy_variables','less','clean');
+gulp.task('build_copy', function () {
+    return gulp.src('./app/fonts/**/*', {base:'./app'})
+        .pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('watch',function(){
-    gulp.watch('./src/*', ['build']);
+gulp.task('build_minify_css', function () {
+    return gulp.src('app/css/*.css')
+        .pipe(concat('vendor.css'))
+        .pipe(minifyCss())
+        .pipe(rename({suffix:'.min'}))
+        .pipe(gulp.dest('dist/css'))
 });
 
-gulp.task('serve',['build'],function(){
-    browserSync.init({
-        server:'./',
-    });
-    gulp.watch('./src/*', ['build']);
+gulp.task('build_html_replace', function() {
+    gulp.src('app/*.html')
+        .pipe(htmlReplace({
+            'css': './css/vendor.min.css',
+        }))
+        .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('dev', function () {
+    runSequence('clean', 'copy_bootstrap', 'copy_theme', ['copy', 'less'], ['browser_sync', 'watch']);
 });
 
 
-
+gulp.task('build', function () {
+    runSequence('clean', 'copy_bootstrap', 'copy_theme', ['copy', 'less'],['build_copy', 'build_minify_css', 'build_html_replace']);
+});
